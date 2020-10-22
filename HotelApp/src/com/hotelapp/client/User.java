73245b8883ec49e053935162dc9bba1a3b92ae73
99002755 <72496610+99002755@users.noreply.github.com>
2020-10-22 18:58:54 +0530

    Add files via upload

---
+++ HotelApp/src/com/hotelapp/client/User.java
@@ -0,0 +1,23 @@
+package com.hotelapp.client;
+
+import java.util.List;
+
+import com.hotelapp.model.Hotel;
+import com.hotelapp.service.HotelService;
+import com.hotelapp.service.HotelServiceImpl;
+
+public class User {
+	public static void main(String[] args) {
+
+	HotelService hotelService = new HotelServiceImpl();
+
+	List<Hotel> list = hotelService.getAllHotels();
+
+	for(Hotel hotel : list) {
+		System.out.println(hotel);
+	}
+	int id = 1;
+	System.out.println(hotelService.getById(id));
+
+}
+}
